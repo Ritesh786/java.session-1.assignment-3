250fd093f2ec8c8c8c2876401bfddfe4af2004f3
Ritesh786 <jharitesh6@gmail.com>
2016-07-13 15:18:16 +0530

    new assignment3
    new assignment3

---
+++ Data.java
@@ -0,0 +1,13 @@
+
+public class Data {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=2;
+		b=3;
+		System.out.println("" +a+b);
+		 a=2.0;   //  both gives error    
+		 b="d";
+		 
+
+}}
